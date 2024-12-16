dfb919153fb9a5c92bc067bce3292b20e635f061
kelly <ensea@StudentLab>
2024-12-16 09:13:37 +0100

    step1 : get argument

---
+++ gettftp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+
+int main(int argc, char *argv[]) {
+	
+    if (argc != 3) {
+        perror("not enough argument");
+        return EXIT_FAILURE;
+    }
+    
+    char *host = argv[1];
+    char *file = argv[2];
+
+
+    return EXIT_SUCCESS;
+}
