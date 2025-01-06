67a74da26984d885744136a48df1d8c111552d6e
kelly <ensea@StudentLab>
2025-01-06 08:45:43 +0100

    step2 : reserve a connection socket to the server

--- puttftp.c
+++ puttftp.c
@@ -9,6 +9,53 @@
 #include <sys/stat.h>
 
 
+/* int socketConnect(char *host, char *port)
+ * 
+ * return socket file descriptor 
+ * 
+ */
+int socketConnect(char *host, char *port) {
+
+    struct addrinfo hints, *result;
+   
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+    hints.ai_flags = 0;
+    hints.ai_protocol = 0;          /* Any protocol */
+    
+    // getaddrinfo
+    int sockfd = getaddrinfo(host, port, &hints, &result);
+    if (sockfd != 0 )
+    {
+		fprintf(stderr,"getaddrinfo: %s\n", gai_strerror(sockfd));
+		exit(EXIT_FAILURE);
+	}
+	
+	/* getaddrinfo returns a list result of structure addrinfo
+	 * Try socket each address until successuful */
+	struct addrinfo *rp;
+	for (rp = result; rp != NULL; rp = rp->ai_next) 
+	{
+		sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+		if (sockfd == -1)
+			continue;
+		else 
+			break;	
+	}
+	if (rp == NULL)
+	{
+	   fprintf(stderr, "Could not connect\n");
+	   exit(EXIT_FAILURE);
+	}
+	
+	freeaddrinfo(result);
+	
+	return sockfd;
+}
+	
+
+
 int main(int argc, char *argv[]) {
 	
     if (argc != 4) {
@@ -19,6 +66,8 @@ int main(int argc, char *argv[]) {
     char *host = argv[1];
     char *port = argv[2];
     char *file = argv[3];
-	
+    
+    int sockfd = socketConnect(host, port);
+    
 }
 
